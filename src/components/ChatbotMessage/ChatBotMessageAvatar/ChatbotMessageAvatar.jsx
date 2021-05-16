43bff206942ede44b820f061f8773df8665833a0
Su Ha Lee <40999647+LeeSuHa98@users.noreply.github.com>
2021-05-16 16:25:22 +0900

    Update ChatbotMessageAvatar.jsx

--- src/components/ChatbotMessage/ChatBotMessageAvatar/ChatbotMessageAvatar.jsx
+++ src/components/ChatbotMessage/ChatBotMessageAvatar/ChatbotMessageAvatar.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import image from './home_img.png'
 const ChatbotMessageAvatar = () => {
   return (
     <div className="react-chatbot-kit-chat-bot-avatar">
       <div className="react-chatbot-kit-chat-bot-avatar-container">
         <div className="react-chatbot-kit-chat-bot-avatar-container">
-        <img src={image}></img>
+        <img src="https://blogfiles.pstatic.net/MjAyMTA1MTZfMTk4/MDAxNjIxMTQ5ODU1OTY5.iGES3rJI37Ug3aaBIXwgjVBHpl2Ydik2Fwt3M-LeKCYg.JJzbkv_dO65LQHMQ0sZhA6oJx6vAp0Cd0ZrUz41Mo6Ig.PNG.lapi12/home_img.png></img>
       </div>
       </div>
     </div>
